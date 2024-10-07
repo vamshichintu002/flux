70951d99f56fe6ffa60167891617084427b07fb8
vamshichintu002 <vamshichintu002@gmail.com>
2024-10-07 18:18:56 +0530

    5

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
-          <SupabaseUserProvider>{children}</SupabaseUserProvider>
-        </body>
+        <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
   )
